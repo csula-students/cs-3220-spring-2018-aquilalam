8eaa90428ed2b83ec0ed50c5543d2706a889dffe
Aquil <aquil_alam@hotmail.com>
2018-03-03 15:38:32 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 class PubSub {
-	constructor () {
-	this.subscribers = [];
-	}
-// subscribe allows a new subscriber to listen for changes by providing
-// callback function in the parameter
-subscribe (fn) {
-	this.subscribers.push(fn);
+    constructor () {
+        this.subscribers = [];
+    }
+
+    // subscribe allows a new subscriber to listen for changes by providing
+    // callback function in the parameter
+    subscribe (fn) {
+        this.subscribers.push(fn);
+    }
+
+    // one can publish any data to subscribers
+    publish (data) {
+        this.subscribers.forEach(subscriber => {
+            subscriber(data);
+        });
+    }
 }
-// one can publish any data to subscribers
-publish (data) {
-	this.subscribes.forEach(subscriber => {
-	subscriber(data);
-	});
-	}
-incre (){
-	window.incrementalGame.state.counter ++;
-}	
-}
+
 const pubSub = new PubSub();
 
-// pubSub.subscriber(data => {
-// 	console.log(data);
-// });
+window.incrementalGame = {
+        state: {
+            counter: 0
+        }
+    };
+	
+	const submit = document.getElementById("ClickMe");
+	var div = document.getElementById("ClickMe");
+	
+	pubSub.subscribe(incr => {
+		window.incrementalGame.state.counter ++;
 
-function increment(){
-	pubSub.incre();
-	document.getElementById("counter").innerHTML = window.incrementalGame.state.counter;
-}
+	})
+	
+	submit.addEventListener('click', () => {
+		pubSub.publish(window.incrementalGame.state.counter);
+		div.textContent = window.incrementalGame.state.counter;
+		console.log(window.incrementalGame.state.counter);
+	});
 
-// pubSub.publish('Hello world!');
+
+
+
